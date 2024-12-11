27253879b140d91971e03a48c6620d1e33713549
oscar <oscar.silva@adsmovil.com>
2024-12-11 00:08:11 -0300

    Ajustes de configuracion

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+};
